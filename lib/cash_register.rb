36e8baf1c67885e502e6577c1ab52d6f3b314948
GINELLE PLAISIR <plaisirginelle@gmail.com>
2020-05-11 07:17:18 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,10 +1,11 @@
 class CashRegister
   
-  attr_reader :discount
+  attr_reader :discount, :items
   attr_accessor :total
   def initialize(discount=20)
     @total = 0
     @discount = discount
+    @items = []
   end
   
   def add_item(title, price, quantity = 1)
