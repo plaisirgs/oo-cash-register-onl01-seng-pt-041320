664d13aabf6c593b3a11090e609c949bff6a0ce2
GINELLE PLAISIR <plaisirginelle@gmail.com>
2020-05-11 06:56:25 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,6 +13,6 @@ class CashRegister
   
   def apply_discount
     @total = @total - @total * @discount/100
-    puts "After the discount, the total comes to "#{total}"."
+    "After the discount, the total comes to "#{total}"."
   end
 end
