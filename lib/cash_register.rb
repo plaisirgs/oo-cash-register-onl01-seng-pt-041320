c041a447ee30cde8abb27019d21f9d7a294bbe38
GINELLE PLAISIR <plaisirginelle@gmail.com>
2020-05-11 07:02:35 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,10 +12,10 @@ class CashRegister
   end
   
   def apply_discount
+     @total = @total - @total * @discount/100
     if @total = 0 
    "There is no discount to apply."
   else
-    @total = @total - @total * @discount/100
     "After the discount, the total comes to $#{@total}."
     end
   end
