a9863651eca1510e7d50cd0d1751f40280909608
GINELLE PLAISIR <plaisirginelle@gmail.com>
2020-05-11 07:20:53 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,10 +9,10 @@ class CashRegister
   end
   
   def add_item(title, price, quantity = 1)
+    @total += price * quantity
     quantity.times do
       @items << title
     end
-    @total += price * quantity
   end
   
   def apply_discount
